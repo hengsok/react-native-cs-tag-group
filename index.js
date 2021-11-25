5864f6b8441d6b64c7e59c93c2f2cd309dd31806
hengsok <imperialhs@outlook.com>
2021-11-25 15:20:14 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Heng Sok 
+ * Forked from https://github.com/aJIEw/react-native-tag-group
+ */
+import TagGroup, { Tag } from './src/TagGroup';
+
+export default TagGroup;
+export { Tag };
